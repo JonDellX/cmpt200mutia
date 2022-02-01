0d098c13829d754d8842a8697ce59dfc6df64903
Wendell <89864599+JonDellX@users.noreply.github.com>
2022-01-31 21:39:43 -0500

    Problem set #1: Find the discriminant

---
+++ hw/1/Problem1.java
@@ -0,0 +1,11 @@
+public class Problem1{
+    public static void main(String[] args) {
+        // Find discriminant of formula 2x2 + 5x + 4 = 0.
+        // Discriminant formula b2 - 4ac ---- b = 5 a = 2  c = 4
+        // 5 * 5 - 4 * 2 * 4
+        
+
+        // Result is -7
+        System.out.println(5 * 5 - 4 * 2 * 4);
+    }
+}
