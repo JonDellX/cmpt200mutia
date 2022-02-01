08e8525a7889fbd4ff33fbb6db27b1de37e5534a
Wendell <89864599+JonDellX@users.noreply.github.com>
2022-01-31 21:56:38 -0500

    Problem #3: Display pattern

---
+++ hw/1/Problem3.java
@@ -0,0 +1,11 @@
+public class Problem3 {
+    public static void main(String[] args) {
+        // Display pattern for big 100 number ----> 103
+        System.out.println("* ***** *****");
+        System.out.println("* *   *     *");
+        System.out.println("* *   * *****");
+        System.out.println("* *   *     *");
+        System.out.println("* ***** *****");
+    }
+    
+}
